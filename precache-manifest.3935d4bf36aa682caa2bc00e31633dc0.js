f6dcbde407e84bc3855da01c5181107c7723d748
Divya Lalwani <divyalalwani1310@gmail.com>
2021-02-15 20:39:57 +0530

    Updates

---
+++ precache-manifest.3935d4bf36aa682caa2bc00e31633dc0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b327dfc71c441af426ceb5acd25008ac",
+    "url": "/movieshark./index.html"
+  },
+  {
+    "revision": "811871707b9a14352383",
+    "url": "/movieshark./static/css/main.ce5f8fca.chunk.css"
+  },
+  {
+    "revision": "b5551f0bd2cfcc71ea8d",
+    "url": "/movieshark./static/js/2.7861cded.chunk.js"
+  },
+  {
+    "revision": "811871707b9a14352383",
+    "url": "/movieshark./static/js/main.87b1d018.chunk.js"
+  },
+  {
+    "revision": "f65e7e76d57c24d94be8",
+    "url": "/movieshark./static/js/runtime~main.29c749fa.js"
+  },
+  {
+    "revision": "149a098f51438682ddb64f6e0c0c709b",
+    "url": "/movieshark./static/media/landingPage.149a098f.jpg"
+  },
+  {
+    "revision": "8562b6565f5ae1db5e4af40d85b4ed2d",
+    "url": "/movieshark./static/media/netflix.8562b656.png"
+  },
+  {
+    "revision": "991556c2526eb97f8e745b79363c8661",
+    "url": "/movieshark./static/media/normal.991556c2.jpg"
+  },
+  {
+    "revision": "c2b89779dd6b85078fc0cc2f3a9aaf28",
+    "url": "/movieshark./static/media/profile.c2b89779.jpg"
+  }
+]);
